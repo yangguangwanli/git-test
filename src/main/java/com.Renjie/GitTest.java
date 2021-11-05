45190a9a6a5bfac0b7440193c2b1f2f14ebd3663
阳光万里 <89236957+yangguangwanli@users.noreply.github.com>
2021-11-05 14:25:11 +0800

    pull commit

--- src/main/java/com.Renjie/GitTest.java
+++ src/main/java/com.Renjie/GitTest.java
@@ -29,5 +29,7 @@ public class GitTest {
         System.out.println("Master test");
         System.out.println("hot-fix test");
         System.out.println("push test");
+        System.out.println("pull test");
+
     }
 }
