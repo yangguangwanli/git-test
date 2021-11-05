a54d2c001f49b823fe787c69f9d7bfc3af715a14
Renjie <yangguangwanli@foxmail.com>
2021-11-05 12:53:19 +0800

    first commit

---
+++ src/main/java/com.Renjie/GitTest.java
@@ -0,0 +1,13 @@
+package main.java.com.Renjie;
+
+/**
+ * @Auther: Renjie
+ * @Date: 2021/11/5 - 12:49
+ * @Description: {PACKAGE_NAME}
+ * @Version: 1.0
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("HelloGit");
+    }
+}
