85981b30c94b86cb2d19b9cb32eb10b1e77612b7
Renjie <yangguangwanli@foxmail.com>
2021-11-05 13:09:00 +0800

    second commit

--- src/main/java/com.Renjie/GitTest.java
+++ src/main/java/com.Renjie/GitTest.java
@@ -3,11 +3,25 @@ package main.java.com.Renjie;
 /**
  * @Auther: Renjie
  * @Date: 2021/11/5 - 12:49
- * @Description: {PACKAGE_NAME}
+ * @Description:
+ * //文件颜色：
+ * 红色：还没被添加进暂存区
+ *
+ * 绿色：已被添加进暂存区
+ *
+ * 浅灰黄色：文件被忽略，未被追踪
+ *
+ * 文件被提交之后，颜色恢复正常的黑色
+ *
+ * 蓝色：被追踪过但后面被修改了
+ *
+ *
  * @Version: 1.0
  */
 public class GitTest {
     public static void main(String[] args) {
         System.out.println("HelloGit");
+
+        System.out.println("HelloGit2222");
     }
 }
